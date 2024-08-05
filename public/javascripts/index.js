31f4b8fc778b20c0edc1a76abf3ed42f3064b9c1
Ajay Karthikesan <113184147+ajayfoo@users.noreply.github.com>
2024-08-05 17:17:05 +0530

    feat: show progress bar on post

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,6 +3,7 @@ const createPostBtn = document.getElementById("create-post");
 const titleTxt = document.getElementById("title");
 const descriptionTxt = document.getElementById("description");
 const progressDialog = document.getElementById("progress-dialog");
+const resultDialog = document.getElementById("result-dialog");
 const postPost = async (title, description) => {
   const response = await fetch(location.href, {
     headers: {
@@ -19,16 +20,28 @@ const postPost = async (title, description) => {
 
 const createPostForm = createPostDialog.querySelector("form:only-of-type");
 
+const showPostCreationResult = (done) => {
+  const result = resultDialog.querySelector("form>.result");
+  if (done) {
+    result.textContent = "Post created successfully";
+  } else {
+    result.textContent = "Failed to create your post";
+  }
+  resultDialog.showModal();
+};
+
 createPostForm.addEventListener("submit", async (e) => {
   if (document.activeElement.id !== createPostBtn.id) return;
   e.preventDefault();
-  progressDialog.showModal();
-  const done = await postPost(titleTxt.value, descriptionTxt.value);
-  if (done) {
-    createPostDialog.close();
-    progressDialog.close();
-    createPostForm.reset();
-  } else {
+  try {
+    progressDialog.showModal();
+    const done = await postPost(titleTxt.value, descriptionTxt.value);
+    if (done) {
+      createPostDialog.close();
+      createPostForm.reset();
+    }
+    showPostCreationResult(done);
+  } finally {
     progressDialog.close();
   }
 });

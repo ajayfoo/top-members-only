1594751587c6cedb4794058f138d152b430ccba2
Ajay Karthikesan <113184147+ajayfoo@users.noreply.github.com>
2024-08-08 14:39:44 +0530

    refactor: refactor admin.js to move all adding event handler statements into a function

--- public/javascripts/index/admin.js
+++ public/javascripts/index/admin.js
@@ -1,43 +1,53 @@
-const postsEle = document.querySelector("main>.posts");
-const postDetailDialog = document.getElementById("post-detail-dialog");
-const allPostElements = document.querySelectorAll(".post");
-const postDeleteForm = postDetailDialog.querySelector("form");
-const deletPostBtn = document.getElementById("delete-post");
-let selectedPostId = null;
+import {
+  attachCommonEventListeners,
+  showProgress,
+  showMessage,
+  hideProgress,
+} from "./utils/common.js";
+import { attachCommonVerifiedEventListeners } from "./utils/verified.js";
 
-allPostElements.forEach((p) => {
-  const username = p.querySelector(".username").textContent;
-  const timestamp = p.querySelector(".timestamp").textContent;
-  const title = p.querySelector(".title").textContent;
-  const description = p.querySelector(".description").textContent;
-  const id = p.dataset.id;
+const deletePostElement = (id) =>
+  document.querySelector(`[data-id="${id}"]`).remove();
 
-  p.addEventListener("click", () => {
-    showPostDetail(username, timestamp, title, description);
-    selectedPostId = id;
+const attachEventListeners = () => {
+  const allPostElements = document.querySelectorAll(".post");
+  let selectedPostId = null;
+  allPostElements.forEach((p) => {
+    const id = p.dataset.id;
+    p.addEventListener("click", () => {
+      selectedPostId = id;
+      console.log(selectedPostId);
+    });
   });
-});
 
-postDeleteForm.addEventListener("submit", async (e) => {
-  e.preventDefault();
-  if (document.activeElement.id !== deletPostBtn.id) {
-    postDetailDialog.close();
-    return;
-  }
-  showProgress("Deleting the post...");
-  const response = await fetch(location.href, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-    method: "DELETE",
-    body: JSON.stringify({ id: selectedPostId }),
+  const postDetailDialog = document.getElementById("post-detail-dialog");
+  const postDeleteForm = postDetailDialog.querySelector("form");
+  const deletPostBtn = document.getElementById("delete-post");
+  postDeleteForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    if (document.activeElement.id !== deletPostBtn.id) {
+      postDetailDialog.close();
+      return;
+    }
+    showProgress("Deleting the post...");
+    const response = await fetch(location.href, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "DELETE",
+      body: JSON.stringify({ id: selectedPostId }),
+    });
+    if (response.ok) {
+      showMessage("Successfully deleted post");
+      hideProgress();
+      postDetailDialog.close();
+      deletePostElement(selectedPostId);
+    } else {
+      showMessage("Failed to delete post");
+    }
   });
-  if (response.ok) {
-    showMessage("Successfully deleted post");
-    hideProgress();
-    postDetailDialog.close();
-    location.reload();
-  } else {
-    showMessage("Failed to delete post");
-  }
-});
+};
+
+attachCommonEventListeners();
+attachCommonVerifiedEventListeners();
+attachEventListeners();
